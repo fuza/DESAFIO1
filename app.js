6e9e9a63e1b82979b601bb653352ebce2f13b97a
fuza <luciano@fuza.com.br>
2025-02-10 18:05:19 -0300

    Atualização funções

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+document.querySelector(".button-add").addEventListener("click", adicionarAmigo);
+document.querySelector(".button-draw").addEventListener("click", sortearAmigo);
+
+function adicionarAmigo(event) {
+    event.preventDefault();  
+
+    const input = document.querySelector("#amigo");
+    const nome = input.value.trim();
+
+    if (nome === "") {
+        alert("Insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+    atualizarLista();
+    input.value = "";  
+}
+
+function atualizarLista() {
+    const lista = document.querySelector("#listaAmigos");
+    lista.innerHTML = amigos.map(nome => `<li>${nome}</li>`).join('');
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Adicione pelo menos um nome para realizar o sorteio!");
+        return;
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const nomeSorteado = amigos[indiceSorteado];
+
+    const resultado = document.querySelector("#resultado");
+    resultado.innerHTML = `O amigo sorteado é: <strong>${nomeSorteado}</strong>`;
+}
